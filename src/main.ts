fb516942551259bbd52e71a7c1f541f62222c47c
Roman Resh <wordsgarden7@gmail.com>
2022-02-09 19:26:02 +0200

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
+
+
+// {
+//   "origin": "*",
+//   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   "preflightContinue": false,
+//   "optionsSuccessStatus": 204
+// }
